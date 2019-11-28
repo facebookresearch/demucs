7bb90d2554f49f9310c5472891316fc275f80f8c
appleholic <choiilji@gmail.com>
2019-11-29 08:47:52 +0900

    change subprocess function from run to getoutput

--- demucs/audio.py
+++ demucs/audio.py
@@ -14,11 +14,8 @@ from .utils import temp_filenames
 
 
 def _read_info(path):
-    process = sp.run(
-        ['ffprobe', str(path), '-print_format', 'json', '-show_format', '-show_streams'],
-        capture_output=True,
-        check=True)
-    return json.loads(process.stdout.decode('utf-8'))
+    stdout_data = sp.getoutput('ffprobe {} -print_format json -show_format -show_streams'.format(str(path)))
+    return json.loads(stdout_data[stdout_data.index('{\n'):])
 
 
 class AudioFile:
