3f480fd966e29a8fa2fa905dd4bbfd009e85dbab
Alexandre Defossez <alexandre.defossez@gmail.com>
2020-01-03 13:14:53 +0100

    Fixed typo

--- demucs/audio.py
+++ demucs/audio.py
@@ -140,15 +140,15 @@ class AudioFile:
                     # Case 2:
                     # The caller asked for multiple channels, but the input file have
                     # one single channel, replicate the audio over all channels.
-                    wav = wav.as_strided(size=(channels, wav.shape[1]), stride = (0,1));
+                    wav = wav.as_strided(size=(channels, wav.shape[1]), stride=(0, 1))
                 elif self.channels() >= channels:
                     # Case 3:
                     # The caller asked for multiple channels, and the input file have
                     # more channels than requested. In that case return the first channels.
-                    wav = wav[:channels,:];
+                    wav = wav[:channels, :]
                 else:
                     # Case 4: What is a reasonable choice here?
-                    raise ValueException('The input file has less channels than requested')
+                    raise ValueError('The input file has less channels than requested')
                 if target_size is not None:
                     wav = wav[..., :target_size]
                 wavs.append(wav)
