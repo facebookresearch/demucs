9ac99e59e43417480c30a51f17549c139f4ae895
Alexandre Defossez <alexandre.defossez@gmail.com>
2022-07-19 11:45:01 +0200

    fixing CI errors

--- demucs/repo.py
+++ demucs/repo.py
@@ -68,7 +68,8 @@ class RemoteRepo(ModelOnlyRepo):
             url = self._models[sig]
         except KeyError:
             raise ModelLoadingError(f'Could not find a pre-trained model with signature {sig}.')
-        pkg = torch.hub.load_state_dict_from_url(url, map_location='cpu', check_hash=True)
+        pkg = torch.hub.load_state_dict_from_url(
+            url, map_location='cpu', check_hash=True)  # type: ignore
         return load_model(pkg)
 
 
