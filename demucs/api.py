9c51bef3a6e9cb2e89c5ee03db6dff87ce6b2b9f
CarlGao4 <gaoweiqicarl1@163.com>
2023-04-25 13:46:44 +0800

    Separation API

--- demucs/api.py
+++ demucs/api.py
@@ -7,19 +7,32 @@
 """API calls
 """
 
+import random
 import subprocess
 import warnings
 
+import torch as th
 import torchaudio as ta
 
-from .apply import BagOfModels
-from .audio import AudioFile, convert_audio, save_audio
+from concurrent.futures import ThreadPoolExecutor
+from typing import Callable, Hashable, Any
+from types import NoneType
+
+from .apply import BagOfModels, tensor_chunk, TensorChunk
+from .audio import AudioFile, convert_audio
 from .pretrained import get_model
 from .separate import get_parser
+from .utils import center_trim, DummyPoolExecutor
 
 
 class LoadAudioError(Exception):
     pass
+
+
+def _replace_dict(_dict: dict, *subs: tuple[Hashable, Any]):
+    for key, value in subs:
+        _dict[key] = value
+    return _dict
 
 
 class Separator:
@@ -75,7 +88,9 @@ class Separator:
         if wav is None:
             raise LoadAudioError(
                 "\n".join(
-                    "When trying to load using {}, got the following error: {}".format(backend, error)
+                    "When trying to load using {}, got the following error: {}".format(
+                        backend, error
+                    )
                     for backend, error in errors.items()
                 )
             )
@@ -109,3 +124,201 @@ class Separator:
         self._file.append(filename)
         self._wav.append(wav)
 
+    def separate_track(
+        self,
+        wav,
+        model=None,
+        segment=0,
+        shifts=0,
+        split=True,
+        overlap=0.25,
+        transition_power=1.0,
+        device=None,
+        num_workers=0,
+        pool=None,
+        callback: Callable[[dict], NoneType] = None,
+        callback_arg: dict = None,
+    ):
+        if device is None:
+            device = self.device
+        if model is None:
+            if self._model is None:
+                raise RuntimeError("Load a model first! ")
+            model = self._model
+        if pool is None:
+            if num_workers > 0 and device.type == "cpu":
+                pool = ThreadPoolExecutor(num_workers)
+            else:
+                pool = DummyPoolExecutor()
+        if callback_arg is None:
+            callback_arg = {}
+        callback_arg = _replace_dict(
+            callback_arg, *{"model_idx_in_bag": 0, "shift_idx": 0, "segment_offset": 0}.items()
+        )
+        kwargs = {
+            "shifts": shifts,
+            "split": split,
+            "overlap": overlap,
+            "transition_power": transition_power,
+            "device": device,
+            "pool": pool,
+            "segment": segment,
+        }
+        if isinstance(model, BagOfModels):
+            estimates = 0
+            totals = [0] * len(model.sources)
+            callback_arg["models"] = len(model.models)
+            for sub_model, weight in zip(model.models, model.weights):
+                original_model_device = next(iter(sub_model.parameters())).device
+                sub_model.to(device)
+
+                out = self.separate_track(
+                    sub_model,
+                    wav,
+                    **kwargs,
+                    callback_arg=callback_arg,
+                    callback=(
+                        lambda d, i=callback_arg["model_idx_in_bag"]: callback(
+                            _replace_dict(d, ("model_idx_in_bag", i))
+                        )
+                    )
+                    if callable(callback)
+                    else None,
+                )
+                sub_model.to(original_model_device)
+                for k, inst_weight in enumerate(weight):
+                    out[:, k, :, :] *= inst_weight
+                    totals[k] += inst_weight
+                estimates += out
+                del out
+                callback_arg["model_idx_in_bag"] += 1
+
+            for k in range(estimates.shape[1]):
+                estimates[:, k, :, :] /= totals[k]
+            return estimates
+
+        callback_arg["models"] = 1
+        model.to(device)
+        model.eval()
+        assert transition_power >= 1, "transition_power < 1 leads to weird behavior."
+        batch, channels, length = wav.shape
+        if shifts:
+            kwargs["shifts"] = 0
+            max_shift = int(0.5 * model.samplerate)
+            wav = tensor_chunk(wav)
+            padded_mix = wav.padded(length + 2 * max_shift)
+            out = 0
+            for shift_idx in range(shifts):
+                offset = random.randint(0, max_shift)
+                shifted = TensorChunk(padded_mix, offset, length + max_shift - offset)
+                shifted_out = self.separate_track(
+                    model,
+                    shifted,
+                    **kwargs,
+                    callback_arg=callback_arg,
+                    callback=(lambda d, i=shift_idx: callback(_replace_dict(d, ("shift_idx", i))))
+                    if callable(callback)
+                    else None,
+                )
+                out += shifted_out[..., max_shift - offset :]
+            out /= shifts
+            return out
+        elif split:
+            kwargs["split"] = False
+            out = th.zeros(batch, len(model.sources), channels, length, device=wav.device)
+            sum_weight = th.zeros(length, device=wav.device)
+            if segment is None:
+                segment = model.segment
+            segment = int(model.samplerate * segment)
+            stride = int((1 - overlap) * segment)
+            offsets = range(0, length, stride)
+            scale = float(format(stride / model.samplerate, ".2f"))
+            # We start from a triangle shaped weight, with maximal weight in the middle
+            # of the segment. Then we normalize and take to the power `transition_power`.
+            # Large values of transition power will lead to sharper transitions.
+            weight = th.cat(
+                [
+                    th.arange(1, segment // 2 + 1, device=device),
+                    th.arange(segment - segment // 2, 0, -1, device=device),
+                ]
+            )
+            assert len(weight) == segment
+            # If the overlap < 50%, this will translate to linear transition when
+            # transition_power is 1.
+            weight = (weight / weight.max()) ** transition_power
+            futures = []
+            for offset in offsets:
+                chunk = TensorChunk(wav, offset, segment)
+                future = pool.submit(
+                    self.separate_track,
+                    model,
+                    chunk,
+                    **kwargs,
+                    callback_arg=callback_arg,
+                    callback=(lambda d, i=offset: callback(_replace_dict(d, ("segment_offset", i))))
+                    if callable(callback)
+                    else None,
+                )
+                futures.append((future, offset))
+                offset += segment
+            for future, offset in futures:
+                chunk_out = future.result()
+                chunk_length = chunk_out.shape[-1]
+                out[..., offset : offset + segment] += (weight[:chunk_length] * chunk_out).to(
+                    wav.device
+                )
+                sum_weight[offset : offset + segment] += weight[:chunk_length].to(wav.device)
+            assert sum_weight.min() > 0
+            out /= sum_weight
+            return out
+        else:
+            if hasattr(model, "valid_length"):
+                valid_length = model.valid_length(length)
+            else:
+                valid_length = length
+            ref = wav.mean(0)
+            wav = (wav - ref.mean()) / ref.std()
+            wav = tensor_chunk(wav)
+            padded_mix = wav.padded(valid_length).to(device)
+            if callable(callback):
+                callback(_replace_dict(callback_arg, ("state", "start")))
+            with th.no_grad():
+                out = model(padded_mix)
+            if callable(callback):
+                callback(_replace_dict(callback_arg, ("state", "end")))
+            return center_trim(out, length) * ref.std() + ref.mean()
+
+    def separate_loaded_track(
+        self,
+        segment=0,
+        split=True,
+        shifts=0,
+        overlap=0.25,
+        transition_power=1.0,
+        device=None,
+        num_workers=0,
+        callback: Callable[[dict], NoneType] = None,
+        callback_arg: dict = None,
+    ):
+        if len(self._file) != len(self._wav):
+            raise RuntimeError("File list and waves not matched. Please `clear_filelist` first. ")
+        kwargs = {
+            "model": self._model,
+            "shifts": shifts,
+            "split": split,
+            "overlap": overlap,
+            "transition_power": transition_power,
+            "device": device,
+            "segment": segment,
+            "num_workers": num_workers,
+        }
+        self._out = []
+        for file, wav in zip(self._file, self._wav):
+            out = self.separate_track(
+                wav,
+                **kwargs,
+                callback_arg=_replace_dict(callback_arg, ("file", file)),
+                callback=callback,
+            )
+            self._out.append(file, dict(zip(out, self._model.sources)))
+            yield file, dict(zip(out, self._model.sources))
